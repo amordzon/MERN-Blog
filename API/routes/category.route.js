e11fc7da85c93187ed93be5a29521fe7971723db
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-12-05 22:01:55 +0100

    Added category CRUD

---
+++ API/routes/category.route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+    getAllCategories,
+    createCategory,
+    getOneCategory,
+    updateCategory,
+    deleteCategory,
+} from '../controllers/category.controller.js';
+
+const categoryRouter = express.Router();
+
+categoryRouter.get('/', getAllCategories);
+categoryRouter.post('/new', createCategory);
+categoryRouter.get('/:categoryid', getOneCategory);
+categoryRouter.put('/:categoryid', updateCategory);
+categoryRouter.delete('/:categoryid', deleteCategory);
+
+export default categoryRouter;

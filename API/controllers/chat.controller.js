d76c31c31136b2979ae751bfb4828ecd82ea4feb
amordzon <97406794+amordzon@users.noreply.github.com>
2022-12-05 23:31:02 +0100

    created chat messages CRUD (#7)

---
+++ API/controllers/chat.controller.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import Chat from '../models/chat.model.js';
+
+export const getAllChatMessages = async (req, res) => {
+    await Chat.find()
+        .populate('user')
+        .then((allChatMessages) => {
+            return res.status(200).json({
+                success: true,
+                message: 'All chat messages',
+                Comments: allChatMessages,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                success: false,
+                message: 'Server error',
+                error: err.message,
+            });
+        });
+};
+
+export const createChatMessage = (req, res) => {
+    const chatmessage = new Chat({
+        _id: mongoose.Types.ObjectId(),
+        user: req.body.user,
+        message: req.body.message,
+        created_at: req.body.created_at,
+    });
+    return chatmessage
+        .save()
+        .then((newChatMessage) => {
+            return res.status(201).json({
+                success: true,
+                message: 'New chat message created successfully',
+                chatMessage: newChatMessage,
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                success: false,
+                message: 'Server error. Please try again.',
+                error: error.message,
+            });
+        });
+};
+
+export const deleteChatMessage = async (req, res) => {
+    const id = req.params.chatmessageid;
+    await Chat.findByIdAndRemove(id)
+        .exec()
+        .then(() =>
+            res.status(204).json({
+                success: true,
+            })
+        )
+        .catch((err) =>
+            res.status(500).json({
+                success: false,
+            })
+        );
+};
--- API/index.js
+++ API/index.js
@@ -5,7 +5,9 @@ import dotenv from 'dotenv';
 import postRouter from './routes/post.route.js';
 import userRouter from './routes/user.route.js';
 import categoryRouter from './routes/category.route.js';
-import categoryComment from './routes/comment.route.js';
+import commentRouter from './routes/comment.route.js';
+import chatRouter from './routes/chat.route.js';
+
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
@@ -32,7 +34,8 @@ mongoose.connect(
 app.use('/api/posts', postRouter);
 app.use('/api/users', userRouter);
 app.use('/api/category', categoryRouter);
-app.use('/api/comments', categoryComment);
+app.use('/api/comments', commentRouter);
+app.use('/api/chat', chatRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Our server is running on port ${process.env.PORT}`);

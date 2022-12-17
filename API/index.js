6881116ee26efdbb537da0eefbc4ca9b8335de72
amordzon <97406794+amordzon@users.noreply.github.com>
2022-12-17 22:37:06 +0100

    Feature: create navbar (#10)
    * created navbar
    
    * added populate virtual to category
    
    * added cors and updated navbar

--- API/index.js
+++ API/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import postRouter from './routes/post.route.js';
 import userRouter from './routes/user.route.js';
@@ -15,6 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 mongoose.connect(
     'mongodb+srv://' +
         process.env.LOGIN +
---
+++ front/src/components/Navbar.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+    return (
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded-b dark:bg-gray-900">
+            <div className="container flex flex-wrap items-center justify-between mx-auto">
+                <a href="https://flowbite.com/" className="flex items-center">
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                        Blog
+                    </span>
+                </a>
+                <div className="flex md:order-2">
+                    <button
+                        type="button"
+                        data-collapse-toggle="navbar-search"
+                        className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+                        onClick={() => setNavbarOpen(!navbarOpen)}
+                    >
+                        <i className="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                    <div className="relative hidden md:block">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <i className="fa fa-search" aria-hidden="true"></i>
+                        </div>
+                        <input
+                            type="text"
+                            id="search-navbar"
+                            className="block w-full p-2 pl-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Szukaj..."
+                        />
+                    </div>
+                    <button
+                        data-collapse-toggle="navbar-search"
+                        type="button"
+                        className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        onClick={() => setNavbarOpen(!navbarOpen)}
+                    >
+                        <i className="fa fa-bars" aria-hidden="true"></i>
+                    </button>
+                    <button
+                        type="button"
+                        data-collapse-toggle="navbar-profile"
+                        className="ml-3 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+                    >
+                        <i className="fa fa-user" aria-hidden="true"></i>
+                    </button>
+                </div>
+                <div
+                    className={
+                        'items-center justify-between ' +
+                        (!navbarOpen && 'hidden') +
+                        ' w-full md:flex md:w-auto md:order-1'
+                    }
+                    id="navbar-search"
+                >
+                    <div className="relative mt-3 md:hidden">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <i
+                                    className="fa fa-search"
+                                    aria-hidden="true"
+                                ></i>
+                            </div>
+                        </div>
+                        <input
+                            type="text"
+                            id="search-navbar"
+                            className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Szukaj..."
+                        />
+                    </div>
+                    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a
+                                href="#"
+                                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                            >
+                                Strona główna
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="#"
+                                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Podróże
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="#"
+                                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Zwierzęta
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="#"
+                                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Inne
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

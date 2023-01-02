a4ef24a91b22d12049701f447f912c84f5454a81
amordzon <97406794+amordzon@users.noreply.github.com>
2023-01-02 21:23:54 +0100

    Feature: remove and update comment (#18)

--- API/routes/comment.route.js
+++ API/routes/comment.route.js
@@ -6,13 +6,14 @@ import {
     updateComment,
     deleteComment,
 } from '../controllers/comment.controller.js';
+import { loggedIn } from '../middleware/auth.middleware.js';
 
 const commentRouter = express.Router();
 
 commentRouter.get('/', getAllComments);
-commentRouter.post('/new', createComment);
+commentRouter.post('/new', [loggedIn], createComment);
 commentRouter.get('/:commentid', getOneComment);
-commentRouter.put('/:commentid', updateComment);
-commentRouter.delete('/:commentid', deleteComment);
+commentRouter.put('/:commentid', [loggedIn], updateComment);
+commentRouter.delete('/:commentid', [loggedIn], deleteComment);
 
 export default commentRouter;

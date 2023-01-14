a3231bf992263c5978d70e1a7aaa7fd53c26742f
Aleksandra Mordzon <aleksandramordzon@Aleksandras-MacBook-Air.local>
2023-01-14 16:37:48 +0100

    save chat to database

--- API/routes/chat.route.js
+++ API/routes/chat.route.js
@@ -1,14 +1,7 @@
 import express from 'express';
-import {
-    getAllChatMessages,
-    createChatMessage,
-    deleteChatMessage,
-} from '../controllers/chat.controller.js';
+import { getAllChatMessages } from '../controllers/chat.controller.js';
 
 const chatRouter = express.Router();
 
 chatRouter.get('/', getAllChatMessages);
-chatRouter.post('/new', createChatMessage);
-chatRouter.delete('/:chatmessageid', deleteChatMessage);
-
 export default chatRouter;

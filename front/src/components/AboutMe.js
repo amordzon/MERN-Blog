24b134faca8e485a5fd3db97ea18e0258049efb0
amordzon <97406794+amordzon@users.noreply.github.com>
2023-04-24 00:21:11 +0200

    Fix: Update AboutMe

--- front/src/components/AboutMe.js
+++ front/src/components/AboutMe.js
@@ -7,8 +7,7 @@ const AboutMe = () => {
             <div className="w-32 h-32 mt-4 mx-auto bg-orange-600 rounded-full"></div>
             <div className="mt-2 text-center font-light text-gray-500">
                 Duis eget imperdiet dolor, sit amet aliquet lorem. Sed sed
-                placerat metus. Pellentesque est nisl, molestie quis finibus
-                vitae.
+                placerat metus. Pellentesque est nisl, nunc porta commodo elit a facilisis. 
             </div>
         </div>
     );
